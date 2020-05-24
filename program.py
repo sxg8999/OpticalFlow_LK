f2f2105d2cbcf6c5630ec964a0be4335476369ae
Steven Guan <sxg8999@gmail.com>
2020-05-24 18:25:14 -0400

    Created a class named Point_Generator that will generate and hold all the points

---
+++ program.py
@@ -0,0 +1,24 @@
+#
+# This is the main program
+#
+# Author: Steven Guan
+#
+
+# The Point_Generator class generates and holds the points (static and moving) 
+class Point_Generator():
+
+    def __init__(self):
+       self.static_points = []
+       self.moving_points = []
+
+
+def main():
+
+    pg = Point_Generator()
+
+    
+
+
+
+if __name__ == "__main__":
+    main()
